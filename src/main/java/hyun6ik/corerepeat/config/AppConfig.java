aff28ed9f9d3f2a17932e3ed8ec42e2e82f56c20
hyun6ik <fkanwm@naver.com>
2022-03-02 16:49:48 +0900

    Refactor #8 : AppConfig - 리펙토링

--- src/main/java/hyun6ik/corerepeat/config/AppConfig.java
+++ src/main/java/hyun6ik/corerepeat/config/AppConfig.java
@@ -1,8 +1,10 @@
 package hyun6ik.corerepeat.config;
 
+import hyun6ik.corerepeat.domain.discount.DiscountPolicy;
 import hyun6ik.corerepeat.domain.member.MemberService;
 import hyun6ik.corerepeat.domain.order.OrderService;
 import hyun6ik.corerepeat.infrastructure.discount.FixDiscountPolicy;
+import hyun6ik.corerepeat.infrastructure.member.MemberRepository;
 import hyun6ik.corerepeat.infrastructure.member.MemberServiceImpl;
 import hyun6ik.corerepeat.infrastructure.member.MemoryMemberRepository;
 import hyun6ik.corerepeat.infrastructure.order.OrderServiceImpl;
@@ -10,11 +12,18 @@ import hyun6ik.corerepeat.infrastructure.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
