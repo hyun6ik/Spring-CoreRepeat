3f725fdbd9f99455198a2c2b6dfdd131f51d86fe
hyun6ik <fkanwm@naver.com>
2022-03-02 14:51:14 +0900

    Initial commit

---
+++ src/main/java/hyun6ik/corerepeat/CoreRepeatApplication.java
@@ -0,0 +1,13 @@
+package hyun6ik.corerepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreRepeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreRepeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hyun6ik/corerepeat/CoreRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package hyun6ik.corerepeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreRepeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
