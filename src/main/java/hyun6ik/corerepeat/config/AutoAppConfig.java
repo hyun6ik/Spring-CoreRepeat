9da589ac0da27e83c214dd37f1282eb32c35d15c
hyun6ik <fkanwm@naver.com>
2022-03-04 13:55:14 +0900

    Add #24 : 중복 등록과 충돌

--- src/main/java/hyun6ik/corerepeat/config/AutoAppConfig.java
+++ src/main/java/hyun6ik/corerepeat/config/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hyun6ik.corerepeat.config;
 
+import hyun6ik.corerepeat.infrastructure.member.MemberRepository;
+import hyun6ik.corerepeat.infrastructure.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,9 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
