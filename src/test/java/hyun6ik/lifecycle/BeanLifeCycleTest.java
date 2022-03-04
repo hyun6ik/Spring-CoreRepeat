d1eddf18eab5b339a9d4e0944142eee6edf61b6d
hyun6ik <fkanwm@naver.com>
2022-03-04 20:53:05 +0900

    Add #30 : 인터페이스 InitializingBean, DisposableBean

---
+++ src/test/java/hyun6ik/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package hyun6ik.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Configuration
+    static class LifeCycleConfig{
+
+        @Bean
+        public NetworkClient networkClient() {
+            final NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+
+    }
+
+    @Test
+    void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        final NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+}
