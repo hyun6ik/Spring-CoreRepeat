4d75a27c3f0fbe7712597902e2d92a26871c18cd
hyun6ik <fkanwm@naver.com>
2022-03-04 16:05:06 +0900

    Add #28 : @Autowired 필드명, @Qualifier, @Primary

--- src/main/java/hyun6ik/corerepeat/infrastructure/discount/RateDiscountPolicy.java
+++ src/main/java/hyun6ik/corerepeat/infrastructure/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package hyun6ik.corerepeat.infrastructure.discount;
 
 import hyun6ik.corerepeat.domain.discount.DiscountPolicy;
 import hyun6ik.corerepeat.domain.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscountPolicy")
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private static int discountPercent = 10;
--- src/main/java/hyun6ik/corerepeat/infrastructure/order/OrderServiceImpl.java
+++ src/main/java/hyun6ik/corerepeat/infrastructure/order/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import hyun6ik.corerepeat.domain.order.Order;
 import hyun6ik.corerepeat.domain.order.OrderService;
 import hyun6ik.corerepeat.infrastructure.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +18,18 @@ public class OrderServiceImpl implements OrderService {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+//    // @Autowired 필드 명 매칭
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = rateDiscountPolicy;
+//    }
+
+//    // @Qualifier
+//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
